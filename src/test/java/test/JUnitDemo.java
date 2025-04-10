92f0a9962b78d243d85479e58c00ded9313c9827
vasuvespag <89766374+vasuvespag@users.noreply.github.com>
2025-04-10 10:39:56 +0530

    Update JUnitDemo.java 10.39 10th Apr

--- src/test/java/test/JUnitDemo.java
+++ src/test/java/test/JUnitDemo.java
@@ -28,6 +28,7 @@ public class JUnitDemo {
 		driver.get("http://adactinhotelapp.com");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		System.out.println("Testing the git pull through cron job");
+		System.out.println("For Jenkins");
 	}
 
 	@After
